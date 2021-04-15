086ba10380f8497d2384a83dba64cf694d83be97
Cauã Bernardino <cbernardino@live.com>
2021-04-14 23:15:09 -0300

    User model and SQL script to generate the DB created.

---
+++ api/src/models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+// User represents the structure of an user data.
+type User struct {
+	ID        uint64    `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Username  string    `json:"username,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedOn time.Time `json:"created_on,omitempty"`
+}
---
+++ database/db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS gocomms;
+USE gocomms;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id int auto_increment primary key,
+    name varchar(100) not null,
+    username varchar(50) not null unique,
+    email varchar(50) not null unique,
+    password varchar(50) not null,
+    created_on timestamp default current_timestamp()
+) ENGINE=INNODB;
