631568326b60ac9110581949334859a524e8bdd5
Cauã Bernardino <cbernardino@live.com>
2021-04-14 22:24:51 -0300

    Configure function created on routes package.

--- api/src/router/routes/routes.go
+++ api/src/router/routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Route represents the structure of the API routes
 type Route struct {
@@ -8,4 +12,15 @@ type Route struct {
 	Method       string
 	Function     func(http.ResponseWriter, *http.Request)
 	AuthRequired bool
+}
+
+// Configure handles the configuration of the routes to be delivered to the router
+func Configure(r *mux.Router) *mux.Router {
+	routes := userRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return r
 }
