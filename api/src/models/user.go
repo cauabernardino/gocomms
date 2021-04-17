0775eff2efeff4197f367bc5f244c9a4048e4e40
Cauã Bernardino <cbernardino@live.com>
2021-04-16 22:00:05 -0300

    Validation functions added to User struct

--- api/src/models/user.go
+++ api/src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // User represents the structure of an user data.
 type User struct {
@@ -10,4 +14,44 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedOn time.Time `json:"created_on,omitempty"`
+}
+
+// Prepare calls the validation and formatting functions for
+// the User struct
+func (user *User) Prepare() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+
+	user.format()
+	return nil
+}
+
+// validate the User struct
+func (user *User) validate() error {
+
+	if user.Name == "" {
+		return errors.New("field name is required")
+	}
+
+	if user.Username == "" {
+		return errors.New("field username is required")
+	}
+
+	if user.Email == "" {
+		return errors.New("field email is required")
+	}
+
+	if user.Password == "" {
+		return errors.New("field password is required")
+	}
+
+	return nil
+}
+
+// formar formats the values for the User struct
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Username = strings.TrimSpace(user.Username)
+	user.Email = strings.TrimSpace(user.Email)
 }
