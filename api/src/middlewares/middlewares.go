62111e9d478d56ec746fb0a98c9909d33d68dd37
Cauã Bernardino <cbernardino@live.com>
2021-04-20 21:33:17 -0300

    Middlewares package created
    - Logger function implemented to send the request info to STDOUT
    - Authenticate function structure created

---
+++ api/src/middlewares/middlewares.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Authenticate verifies if the user making the request is authenticated
+func Authenticate(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Authenticating...")
+		next(w, r)
+	}
+}
+
+// Logger writes the request information to the STDOUT
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("\n%s %s %s", r.Method, r.RequestURI, r.Host)
+		next(w, r)
+	}
+}
