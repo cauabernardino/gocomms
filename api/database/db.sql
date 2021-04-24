7a271990189962bf0c9550a4f29a72275659ba29
Cauã Bernardino <cbernardino@live.com>
2021-04-24 13:40:36 -0300

    Created model and db table for posts

--- api/database/db.sql
+++ api/database/db.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE IF NOT EXISTS gocomms;
 USE gocomms;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id int auto_increment primary key,
@@ -25,4 +26,19 @@ CREATE TABLE followers (
     ON DELETE CASCADE,
 
     primary key(user_id, follower_id)
+) ENGINE=INNODB;
+
+
+CREATE TABLE posts(
+    id int auto_increment primary key,
+    title varchar(100) not null,
+    content varchar(360) not null,
+
+    author_id int not null,
+    FOREIGN KEY (author_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+    likes int default 0,
+    created_on timestamp default current_timestamp()
 ) ENGINE=INNODB;
