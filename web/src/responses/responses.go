27c16e9459ecaa6b67839383242c12c6d5215d34
Cauã Bernardino <cbernardino@live.com>
2021-04-30 12:47:15 -0300

    Refactor on ReturnJSON func and scripts position.

--- web/src/responses/responses.go
+++ web/src/responses/responses.go
@@ -16,7 +16,7 @@ func ReturnJSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
-	if data != nil {
+	if statusCode != http.StatusNoContent {
 		if err := json.NewEncoder(w).Encode(data); err != nil {
 			log.Fatal(err)
 		}
