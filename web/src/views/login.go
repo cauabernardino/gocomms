065b4b03113e3a719cb9b07368f0ebfe8c34b350
Cauã Bernardino <cbernardino@live.com>
2021-05-01 12:23:56 -0300

    Check for logged user for redirecting from /login

--- web/src/views/login.go
+++ web/src/views/login.go
@@ -2,9 +2,15 @@ package views
 
 import (
 	"net/http"
+	"web/src/utils"
 )
 
 // LoginPage handles the loading of login page
 func LoginPage(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := utils.CheckCookie(r)
+	if cookie["token"] != "" {
+		http.Redirect(w, r, "/home", http.StatusFound)
+		return
+	}
 	RenderTemplate(w, "login.html", nil)
 }
